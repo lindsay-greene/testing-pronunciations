e8b9e9d6a97076fdf076de5e32b001a97a0ce3e6
lindsay-greene <lindsay.smile@yahoo.com>
2020-10-30 15:19:12 -0400

    wordnik logic

--- main.js
+++ main.js
@@ -23,3 +23,23 @@ ahd.onclick = function() {
     var utterance = new SpeechSynthesisUtterance("ăp′əl");
     synth.speak(utterance);
 }
+
+
+var baseUrl = "https://api.wordnik.com/v4/word.json/"
+var apiKey = "i6ywqo6rwijjztu6tr3y6ackzvib0hdrit0mvjcx5brehzt97";
+var word = "apple";
+var apiUrl = baseUrl + word + "/pronunciations?useCanonical=false&typeFormat=ahd&limit=50&api_key=" + apiKey;
+console.log(apiUrl);
+
+let response = fetch(apiUrl);
+fetch(apiUrl) 
+    .then(response => {
+        response.text
+        // for set in response 
+            // if set contains raw type : IPA then store it and break loop 
+            // else break it down using our own regex OR just say no pronunciation available 
+        // ipa = storedtext.raw
+        // synth.speak(ipa)
+    });
+    console.log(response);
+    
