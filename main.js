7b777a3f7a54bbb7be390648e21d42bced34eb40
lindsay-greene <lindsay.smile@yahoo.com>
2020-11-06 00:12:27 -0500

    using http request

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 
-var entry = document.getElementById("entry");
-var form = document.getElementById("form");
+//var entry = document.getElementById("entry");
+//var form = document.getElementById("form");
 var submit = document.getElementById("submit");
 console.log(entry);
 
+var apiURL = "";
 submit.onclick = function() {
     console.log(form);
-    let word = entry.innerText;
+    let word = document.getElementById("entry").value;
     var baseUrl = "https://api.wordnik.com/v4/word.json/"
     var apiKey = "i6ywqo6rwijjztu6tr3y6ackzvib0hdrit0mvjcx5brehzt97";
-    var apiUrl = baseUrl + word + "/pronunciations?useCanonical=false&typeFormat=ahd&limit=50&api_key=" + apiKey;
+    apiUrl = baseUrl + word + "/pronunciations?useCanonical=false&typeFormat=ahd&limit=50&api_key=" + apiKey;
     console.log(apiUrl);
 
-    let response = fetch(apiUrl);
-    fetch(apiUrl) 
-    .then(response => {
-        response.text
-        // find rawtype:IPA and store parent element
-        // ipa = storedtext.raw
-        // synth.speak(ipa)
-    });
-    console.log(response.text);
+    let request = new XMLHttpRequest();
+    request.open("GET", apiURL);
+    request.responseType = "json";
+    request.send();
+    console.log(request);
 
+    var pronunciations;
+    request.onload = function() {
+        pronunciations = request.responseXML;
+        console.log("test");
+        console.log(pronunciations);
+    }
 }
-
-
-
     
