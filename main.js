46359907993db5dc90e6dd260f37fedbf7115867
lindsay-greene <lindsay.smile@yahoo.com>
2020-11-11 00:22:11 -0500

    async setup / decouple functionality from user action

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
 
-//var entry = document.getElementById("entry");
-//var form = document.getElementById("form");
-var submit = document.getElementById("submit");
+var entry = document.getElementById("entry");
 console.log(entry);
 
 var apiURL = "";
-submit.onclick = function() {
-    console.log(form);
+
+submit.addEventListener("keyup", function(event){
+    if(event.keyCode == 13) {
+        event.preventDefault;
+    }
+});
+
+async function buildURL(event) {
     let word = document.getElementById("entry").value;
     var baseUrl = "https://api.wordnik.com/v4/word.json/"
     var apiKey = "i6ywqo6rwijjztu6tr3y6ackzvib0hdrit0mvjcx5brehzt97";
     apiUrl = baseUrl + word + "/pronunciations?useCanonical=false&typeFormat=ahd&limit=50&api_key=" + apiKey;
     console.log(apiUrl);
+}
 
+async function sendRequest(event) {
+    await buildURL;
     let request = new XMLHttpRequest();
     request.open("GET", apiURL);
     request.responseType = "json";
     request.send();
     console.log(request);
+}
 
-    var pronunciations;
-    request.onload = function() {
-        pronunciations = request.responseXML;
-        console.log("test");
-        console.log(pronunciations);
-    }
+async function readData(event) {
+    await sendRequest;
+    // take in data 
+    // find "rawtype":"IPA"
+    // get its parent element
+    // search parent element for "raw" value
+}
+
+async function speak(pronunciation) {
+    await readData;
+    speechSynthesis.speak(pronunciation)
 }
     
